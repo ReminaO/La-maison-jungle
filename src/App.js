d7cc43048679643dfb7005eb461dd6dfa20ccbc9
ReminaO <oremina@hotmail.fr>
2021-12-09 15:53:06 +0100

    Finalisation du code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { useState, useEffect } from 'react'
+
 import Banner from './components/Banner'
 import logo from './assets/logo.png'
-// import Cart from './Cart'
+import Cart from './components/Cart'
 import ShoppingList from './components/ShoppingList'
+import Footer from './components/Footer'
+import './styles/Layout.css'
 
 function App() {
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
 	return (
 		<div>
 			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-			{/* <Cart /> */}
-			<ShoppingList />
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
+			<Footer />
 		</div>
 	)
 }
