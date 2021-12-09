01f3b4e955455faea2bd1681a21c5807c6e07a76
ReminaO <oremina@hotmail.fr>
2021-12-09 12:53:38 +0100

    ajout composant PlantItem

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-
-import React from 'react';
 import Banner from './components/Banner'
-import Cart from './components/Cart';
-import ShoppingList from './components/ShoppingList';
-
+import logo from './assets/logo.png'
+// import Cart from './Cart'
+import ShoppingList from './components/ShoppingList'
 
 function App() {
-  return (
-    <div>
-      <Banner />
-      <Cart />
-      <ShoppingList />
-    </div>
-  )
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			{/* <Cart /> */}
+			<ShoppingList />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,33 +1,34 @@
-import '../styles/ShoppingList.css';
-
 import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
-    const categories = plantList.reduce(
+	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
 		[]
 	)
 
 	return (
-		<div >
+		<div>
 			<ul>
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul className="lmj-plant-list">
-				{plantList.map((plant) => (
-                    <li className="lmj-plant-item" key={plant.id}>
-                        {plant.name}
-                        {plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
-                    </li>
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
 			</ul>
 		</div>
 	)
 }
-
-
 
 export default ShoppingList
